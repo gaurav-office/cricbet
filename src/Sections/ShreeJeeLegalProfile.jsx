5fcf9c6e625b63edd3d7d76e3dc94609f5421bbd
badMysteri-o <bhshar678@gmail.com>
2025-03-04 19:59:45 +0530

    Updated code and added new files

---
+++ src/Sections/ShreeJeeLegalProfile.jsx
@@ -0,0 +1,193 @@
+const ShreeJeeLegalProfile = () => {
+  return (
+    <div className="pt-24 bg-gray-50 min-h-screen font-montserrat relative">
+      <div className=" absolute inset-0 bg-slate-300 bg-opacity-30 blur-sm"></div>
+      <header className=" text-white p-6  shadow-md relative z-10">
+        <div className=" container mx-auto text-center"></div>
+      </header>
+
+      <main className="container mx-auto p-8 relative z-10 max-w-4xl overflow-y-auto h-[80vh]">
+        <div className="grid grid-cols-1 gap-6">
+          <section className="bg-white p-6 shadow-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4 text-center">Advocates</h2>
+            <ul className="list-disc pl-6 text-lg text-center">
+              <li>Mr. Rachit Sharma</li>
+              <li>Mr. Shashank Pancholi</li>
+              <li>Mr. Hanumant Singh Shekhawat</li>
+            </ul>
+          </section>
+          <section className="bg-white p-6 shadow-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              PRACTISE AREAS
+            </h2>
+            <p className="text-justify">
+              Our core practice areas are Arbitration & Dispute Resolution,
+              Capital Market, Private Equity, Corporate & Commercial Laws,
+              Corporate Governance, Dispute Resolution Consultancy, E commerce,
+              Foreign Direct Investment, Insolvency & Bankruptcy, Joint venture
+              & Technical Collaborations, FEMA Consultancy, Corporate
+              Negotiations, Legal Risk Management, Legal Due Diligence,
+              Compromise and Arrangement, Private Equity & Venture Capital,
+              Privatization & Disinvestments, Project & Corporate Finance, Real
+              Estate, Corporate Restructuring, Regulatory Approvals, Taxation
+              (Direct & Indirect & International), Drafting of agreements, Legal
+              documentation, Valuation, Benami & Money Laundering Litigation.
+              Our presence is before various Courts, Tribunal, forums and
+              statutory bodies viz. Rajasthan High Court, District Court,
+              Commercial Court, Arbitral Tribunals, National Company Law
+              Tribunal (NCLT)/National Company Law Appellate Tribunal (NCLAT),
+              Debt Recovery Tribunal / Debt Recovery Appellate Tribunal
+              (DRT/DRAT), Income Tax Appellate Tribunal (ITAT), Customs Excise
+              and Service Tax Appellate Tribunal (CESTAT), Central
+              Administrative Tribunal (CAT), Consumer Forum including National
+              Consumer Dispute Redressal Commission (NCDRC), Micro Small
+              Enterprises Facilitation Council, Real Estate regulatory Authority
+              (RERA),Tax Board, Enforcement Directorate.
+            </p>
+          </section>
+
+          <section className="bg-white p-6 shadow-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Litigation and Dispute Resolution
+            </h2>
+            <ul className="list-disc pl-6 text-lg text-justify">
+              <li>
+                ✔ Commercial Disputes (Dispute Resolution under Companies Act,
+                2013, Arbitration and Conciliation Act, 1996, MSMED Act, 2006,
+                Civil Dispute under Code of Civil Procedure, 1908 read with
+                Commercial Court Act, Insolvency and Bankruptcy Code, 2016 etc.)
+              </li>
+              <li>✔ Consumer Disputes</li>
+              <li>✔ Corporate Insolvency Resolution</li>
+              <li>✔ Civil Disputes</li>
+              <li>✔ Violation of Intellectual Property Rights</li>
+              <li>✔ Cross Border Litigation and Investigations</li>
+              <li>✔ Domestic and International Arbitrations</li>
+              <li>✔ Micro Small Enterprises Litigation</li>
+              <li>✔ Property Laws</li>
+              <li>✔ Real Estate Litigation</li>
+              <li>✔ Recovery proceedings under various Laws</li>
+              <li>✔ Tax Litigation (Direct/Indirect)</li>
+              <li>
+                ✔ Departmental Inquiry, Investigation, Adjudication Notices etc.
+              </li>
+              <li>
+                ✔ Cases under SARFAESI Act, 2002, The Recovery of Debts due to
+                Banks and Financial Institutions Act, 1993
+              </li>
+              <li>
+                ✔ Cases under RTPP Act, 2012, Electricity Cases before RERC,
+                Motor Vehicle Act, Cases under SEBI Act etc.
+              </li>
+            </ul>
+          </section>
+
+          <section className="bg-white p-6 shadow-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Criminal Litigation
+            </h2>
+            <ul className="list-disc pl-6 text-lg text-justify">
+              <li>✔ Benami Transactions Frauds</li>
+              <li>✔ Corporate Frauds</li>
+              <li>✔ Criminal Complaints</li>
+              <li>✔ Criminal Trial</li>
+              <li>✔ Criminal Writs / Petitions / Revisions / Appeals</li>
+              <li>✔ Defamation Cases</li>
+              <li>✔ Dishonour of Cheques</li>
+              <li>✔ Misappropriation and Siphoning off of Funds</li>
+              <li>✔ Narcotics and Drugs Violation</li>
+              <li>✔ Prevention of Corruption Act</li>
+              <li>✔ Prevention of Money Laundering Act</li>
+              <li>✔ Property fraud cases</li>
+              <li>✔ Quashing of FIRs / Trial</li>
+              <li>✔ Bails (Regular / Anticipatory)</li>
+              <li>✔ Serious Frauds Investigation office</li>
+              <li>✔ White Collar Crimes including Economic Offences</li>
+            </ul>
+          </section>
+
+          <section className="bg-white p-6 shadow-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Non-Litigation: Consultancy and Advisory Services
+            </h2>
+            <ul className="list-disc pl-6 text-lg text-justify">
+              <li>
+                ✔ All aspects of commercial and company related affairs,
+                including advice, drafting and structuring corporate
+                transactions and business contracts, shareholders agreements,
+                share purchase agreements, incorporation of companies,
+                takeovers, mergers & de-mergers, acquisitions, amalgamations,
+                management buy-outs, equity related transactions like venture
+                capital funding, private equity funding, joint ventures, etc.,
+                and other commercial contracts
+              </li>
+              <li>
+                ✔ Insolvency Professional support - Valuation, Forensic
+                Advisory, Liquidation realisation and Resolution Plan
+              </li>
+              <li>✔ Consultancy in the matter of Transfer Pricing</li>
+              <li>✔ Financial Advisory</li>
+              <li>✔ Business Valuation</li>
+              <li>✔ Investment Management under FEMA Act</li>
+              <li>
+                ✔ Distress Asset Management and Consultancy and helping
+                defaulters in Negotiating terms with bankers/financial
+                institutions
+              </li>
+              <li>✔ Real Estate Management Planning under RERA</li>
+              <li>
+                ✔ Drafting of Legal Opinion under Companies Act, 2013, FEMA,
+                1999 and other allied laws
+              </li>
+              <li>
+                ✔ Restructuring of Partnership firm considering taxation
+                concerns
+              </li>
+              <li>✔ Managing Financial and Legal Risks</li>
+              <li>
+                ✔ Due Diligence of prospective investee companies including Risk
+                analysis in case of Private Equity Investment and compliances as
+                per Company Act, 2013
+              </li>
+              <li>
+                ✔ Legal Due Diligence in case of Companies proposed for IPO and
+                FPO as per SEBI Act
+              </li>
+              <li>
+                ✔ Advice and representing multinational corporations, financial
+                institutions and trade associations on foreign direct investment
+                into India
+              </li>
+              <li>
+                ✔ Advising Foreign Clients to set up their business entity in
+                India
+              </li>
+            </ul>
+          </section>
+
+          <section className="bg-white p-6 shadow-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Contact Information
+            </h2>
+            <address className="text-lg text-gray-700 text-center">
+              <p>
+                Ground Floor, H-15, Chitranjan Marg, C-Scheme, Jaipur-302001,
+                Rajasthan
+              </p>
+              <p>Phone: +91 7737662510</p>
+              <p>Email: contact@shreejeelegal.in</p>
+              <p>
+                Website:
+                <a href="#" className="text-blue-600 underline">
+                  www.shreejeelegal.in
+                </a>
+              </p>
+            </address>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default ShreeJeeLegalProfile;
