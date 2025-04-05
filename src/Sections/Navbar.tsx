e83621e64213f5ae2859911e4219f1284e923e4f
gaurav-office <Gaurav.yashikainfotech@gmail.com>
2025-04-05 13:10:39 +0530

    Initial commit

--- src/Sections/Navbar.tsx
+++ src/Sections/Navbar.tsx
@@ -13,19 +13,19 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="w-full bg-black text-yellow-400 px-4 py-3">
-      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center">
-        <h1 className="text-3xl font-bold mb-3 sm:mb-0">CRICBET99</h1>
-        <div className="space-y-2 sm:space-y-0 sm:space-x-4 flex flex-col sm:flex-row">
+    <nav className="w-full bg-black text-yellow-400 px-3 py-2">
+      <div className="max-w-7xl mx-auto flex flex-row justify-between items-center">
+        <h1 className="text-xl sm:text-3xl font-bold">CRICBET99</h1>
+        <div className="flex space-x-2 sm:space-x-4">
           <button
             onClick={toLoginpage}
-            className="bg-yellow-400 text-black px-4 py-2 rounded hover:bg-yellow-300 transition"
+            className="text-xs sm:text-base bg-yellow-400 text-black px-2 py-1 sm:px-4 sm:py-2 rounded hover:bg-yellow-300 transition"
           >
             Login
           </button>
           <button
             onClick={handleSignupRedirect}
-            className="bg-yellow-400 text-black px-4 py-2 rounded hover:bg-yellow-300 transition"
+            className="text-xs sm:text-base bg-yellow-400 text-black px-2 py-1 sm:px-4 sm:py-2 rounded hover:bg-yellow-300 transition"
           >
             Signup
           </button>
